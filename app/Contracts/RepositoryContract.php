5e472ce13e50f5e23d7cc3494e557699e51bf327
Chappie <ugurtosun50@gmail.com>
2020-07-11 16:45:56 +0300

    Updated

--- app/Contracts/RepositoryContract.php
+++ app/Contracts/RepositoryContract.php
@@ -6,4 +6,23 @@ namespace App\Contracts;
 interface RepositoryContract
 {
 
+    public function __construct();
+
+    public function all();
+
+    public function get($columns = ['*']);
+
+    public function paginate($perPage = 15, $columns = ['*'], $pageName = 'page', $page = null);
+
+    public function find($id, $columns = ['*']);
+
+    public function findOrFail($id, $columns = ['*']);
+
+    public function store(array $data);
+
+    public function update(array $data, $id);
+
+    public function destroy($id);
+
+    public function exists($id);
 }

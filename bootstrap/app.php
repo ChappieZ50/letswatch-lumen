674138136266cd0b3327f93378e2753708c5839d
Chappie <ugurtosun50@gmail.com>
2020-07-11 16:48:18 +0300

    Event Service Provider activated.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -98,7 +98,7 @@ $app->middleware([
 
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
 $app->register(Illuminate\Redis\RedisServiceProvider::class);
 $app->register(Fruitcake\Cors\CorsServiceProvider::class);
 

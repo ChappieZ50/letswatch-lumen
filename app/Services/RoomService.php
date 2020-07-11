5166e76a079c085c7f8e9c5baf1ca8a8a3364e79
Chappie <ugurtosun50@gmail.com>
2020-07-11 16:49:20 +0300

    Save pattern changed. UserInRoom and user "exists" methods added.

--- app/Services/RoomService.php
+++ app/Services/RoomService.php
@@ -16,7 +16,7 @@ class RoomService
 
     public function createExpire()
     {
-        return 3600 * 24; // 24 Hours
+        return 3600 * 12; // 24 Hours
     }
 
     public function rules()
@@ -31,24 +31,45 @@ class RoomService
 
     public function save(Request $request)
     {
-        $uuid = $this->createHash();
+        $room_id = $this->createHash();
         $expire = $this->createExpire();
 
-        $request->merge([
-            'room_id' => $uuid,
-        ]);
-
-        return app('redis')->set($request->get('room_id'), json_encode([
-            'room_id' => $request->get('room_id'),
-            'owner'   => $request->get('user_id'),
-            'users'   => [
+        return app('redis')->set($room_id, json_encode([
+            'room_id'  => $room_id,
+            'owner_id' => $request->get('user_id'),
+            'users'    => [
                 [
                     'username' => $request->get('username'),
                     'user_id'  => $request->get('user_id'),
                     'gender'   => $request->get('gender'),
+                    'owner'    => true,
                 ]
             ],
-        ]), 'ex', $expire) ? app('redis')->get($uuid) : false;
+            'player'   => [
+                'url'  => 'https://youtu.be/UjtOGPJ0URM',
+                'type' => 'youtube',
+                'seek' => 0
+            ]
+        ]), 'ex', $expire) ? app('redis')->get($room_id) : false;
     }
 
+    public function userInRoom($roomID, $userID)
+    {
+        $room = json_decode(app('redis')->get($roomID));
+        if (isset($room->users) && !empty($room->users)) {
+            foreach ($room->users as $user) {
+                if ($user->user_id === $userID) {
+                    return true;
+                    break;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function exists($roomId)
+    {
+        $room = app('redis')->get($roomId);
+        return !empty($room) ? json_decode($room) : false;
+    }
 }

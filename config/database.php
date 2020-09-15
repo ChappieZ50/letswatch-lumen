9b445e3357a951a80943bcddcb0fff0da653ad21
Chappie <ugurtosun50@gmail.com>
2020-09-15 22:31:27 +0300

    Chat database added

--- config/database.php
+++ config/database.php
@@ -19,5 +19,11 @@ return [
             'port'     => env('REDIS_PORT', 6379),
             'database' => 1,
         ],
+        'chat'    => [
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => 2,
+        ]
     ],
 ];
